3e8e4951c5e4ed3106426cdbcd42ba0b3a7691cc
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-16 00:01:18 +0300

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const imagesGallery = document.querySelector(".gallery");
+console.log(imagesGallery);
+
+const imagesGalleryEl = images.map(
+    (image) => `<li><img src=${image.url} alt='${image.alt}' width=320px/></li>`
+  )
+  .join("");
+console.log(images);
+imagesGallery.insertAdjacentHTML("beforeend", imagesGalleryEl);
