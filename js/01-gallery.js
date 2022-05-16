741da9d65d41a4351f328215d70f8a9629eabc33
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-16 22:48:10 +0300

    part task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,41 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+console.log('galleryList:>> ', galleryList);
+const galleryItemsList = galleryItems;
 
-const imagesGallery = document.querySelector(".gallery");
-console.log(imagesGallery);
+const imagesList = galleryItemsList.map(element => {
+  const {preview, original, description} = element;
 
-const imagesGalleryEl = images.map(
-    (image) => `<li><img src=${image.url} alt='${image.alt}' width=320px/></li>`
-  )
-  .join("");
-console.log(images);
-imagesGallery.insertAdjacentHTML("beforeend", imagesGalleryEl);
+  return `<div class="gallery">
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+  />
+</a>  
+  </div>`;
+ })
+ .join('');
+
+ galleryList.insertAdjacentHTML('afterbegin', imagesList);
+console.log('imagesList :>>', imagesList);
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+const instance = imagesList.create(imagesList);
+
+instance.show()
+
+// const galeryItemList = galleryItems;
+// console.log('object :>> ', galeryItemList);
+// const imagesGallery = document.querySelector(".gallery");
+// console.log(imagesGallery);
+
+// const imagesGalleryEl = galleryItems.map({image} => 
+  
+//   console.log(image));
+
