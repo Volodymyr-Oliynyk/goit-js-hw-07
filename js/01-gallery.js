9692e04e3fdab6dc6f0af781143536c08434161a
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-18 00:01:48 +0300

    task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const galleryList = document.querySelector('.gallery');
+const galleryList = document.querySelector(".gallery");
 const galleryItemsList = galleryItems;
 
-const imagesList = galleryItemsList.map(element => {
-  const {preview, original, description} = element;
+const imagesList = galleryItemsList
+  .map((element) => {
+    const { preview, original, description } = element;
 
-  return `<div class="gallery">
+    return `<div class="gallery">
   <a class="gallery__link" href="${original}">
   <img
   class="gallery__image"
@@ -16,40 +17,41 @@ const imagesList = galleryItemsList.map(element => {
   />
 </a>  
   </div>`;
- })
- .join('');
+  })
+  .join("");
 
- galleryList.insertAdjacentHTML('afterbegin', imagesList);
- 
- galleryList.addEventListener('click', openModalClick);
+galleryList.insertAdjacentHTML("afterbegin", imagesList);
 
- let instance = null;
+galleryList.addEventListener("click", openModalClick);
 
+let instance = null;
 
- function openModalClick (event) {
-   event.preventDefault()
-   if (event.target.nodeName !== 'IMG'){
-     return;
-   }
-   
-   instance = basicLightbox.create(`<div> 
-   <img src=${event.target.dataset.source}>
-   </div>`,
+function openModalClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
 
- {onShow : ()  =>  {
-   window.addEventListener('keydown', onEscapePress);
- } ,
+  instance = basicLightbox.create(
+    `<div> 
+    <img src=${event.target.dataset.source}>
+    </div>`,
 
- onClose : ()  =>  { 
-  window.removeEventListener('keydown', onEscapePress);
- }
-} );
- instance.show();
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscapePress);
+      },
+
+      onClose: () => {
+        window.removeEventListener("keydown", onEscapePress);
+      },
+    }
+  );
+  instance.show();
 }
- 
- function onEscapePress (event) {
-if (event.code === 'Escape'){
-  instance.close();
+
+function onEscapePress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+  }
 }
- }
- 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+const galleryItemsList = galleryItems;
 
-console.log(galleryItems);
+const imagesList = galleryItemsList
+  .map((element) => {
+    const { preview, original, description } = element;
+
+    return `<li>
+  <a class="gallery__link" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+   alt="${description}"
+  />
+</a> 
+</li>`;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", imagesList);
+
+const lightbox = new SimpleLightbox("gallery a", {
+  captionsData: "alt",
+  captionDelay: "250ms",
+});
+console.log(lightbox);
